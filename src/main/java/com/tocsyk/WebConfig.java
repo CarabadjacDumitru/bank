bc1ffc8e67f3293968a005255fb69cafa747fab6
CarabadjacDumitru <carabadjacdumitru@gmail.com>
2017-09-06 18:38:10 +0300

    work 2017 09 06

---
+++ src/main/java/com/tocsyk/WebConfig.java
@@ -0,0 +1,37 @@
+package com.tocsyk;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.tocsyk")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver configureViewResolver() {
+        InternalResourceViewResolver viewResolve = new InternalResourceViewResolver();
+        viewResolve.setPrefix("/WEB-INF/views/");
+        viewResolve.setSuffix(".jsp");
+
+        return viewResolve;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer){
+        configurer.enable();
+    }
+}
